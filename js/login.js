962a3edc680171c1ccf93bae51d4ad561bf5c859
Martin Unger <martin-unger2@live.at>
2023-09-02 10:56:12 +0200

    Sign Up, Forgot Password Design

--- js/login.js
+++ js/login.js
@@ -1,51 +1,70 @@
+
 function loggedIn() {
     window.location.href = "summary.html";
 }
 
 
+function backToLogin() {
+    window.location.href = "login.html";
+    document.getElementById("signUp").classList.remove("d-none");
+}
+
+
 function signInTemplate() {
-    document.getElementById("loginBox").innerHTML = `
-    <h1>Sign Up</h1>
-    <div class="separator m-32"></div>
-    <div class="inputFields m-32 d-flex justify-content-between align-items-center">
-        <input required type="text" placeholder="Name"/>
-        <div class="imgBox d-flex justify-content-center align-items-center">
-        <img src="../img/person.svg" alt="">
-        </div>          
-    </div>
-    <div class="inputFields m-32 d-flex justify-content-between align-items-center">
-        <input required type="email" placeholder="Email"/>
-        <div class="imgBox d-flex justify-content-center align-items-center">
-        <img src="../img/mail.svg" alt="">
-        </div>          
-    </div>
-    <div class="inputFields m-32 d-flex justify-content-between align-items-center">
-        <input required type="password" placeholder="Password"/>
-        <div class="imgBox d-flex justify-content-center align-items-center">
-        <img src="../img/lock.svg" alt="">
+    document.getElementById("signUp").classList.add("d-none");
+    document.getElementById("loginBox").innerHTML = /*html*/ `
+    <div class="backImg">
+        <img  onclick="backToLogin()" src="../img/Vector.svg" alt="">
+    </div> 
+    <form onsubmit="validatePassword()" class="d-flex justify-content-center flex-column align-items-center">
+        <h1 class="h1WithBack">Sign Up</h1>
+        <div class="separator m-32"></div>
+        <div class="inputFields m-32 d-flex justify-content-between align-items-center">
+            <input required type="text" placeholder="Name"/>
+            <div class="imgBox d-flex justify-content-center align-items-center">
+            <img src="../img/person.svg" alt="">
+            </div>          
         </div>
-    </div>
-    <div class="inputFields m-32 d-flex justify-content-between align-items-center">
-        <input required type="password" placeholder="Confirm Password"/>
-        <div class="imgBox d-flex justify-content-center align-items-center">
-        <img src="../img/lock.svg" alt="">
+        <div class="inputFields m-32 d-flex justify-content-between align-items-center">
+            <input required type="email" placeholder="Email"/>
+            <div class="imgBox d-flex justify-content-center align-items-center">
+            <img src="../img/mail.svg" alt="">
+            </div>          
         </div>
-    </div>
-    <div class="rememberForgot m-32 d-flex justify-content-evenly align-items-center">
-        <div class="d-flex">
-        <img src="../img/Check button.svg" alt="">
-        <p>I accept the <b class="blueFont">Privacy Policy</b></p>
+        <div class="inputFields m-32 d-flex justify-content-between align-items-center">
+            <input required type="password" placeholder="Password" id="password"/>
+            <div class="imgBox d-flex justify-content-center align-items-center">
+            <img src="../img/lock.svg" alt="">
+            </div>
         </div>
-    </div>
-    <button onclick="loggedIn()" class="btnDark m-32">Sign up</button>
+        <div class="inputFields m-32 d-flex justify-content-between align-items-center">
+            <input required type="password" placeholder="Confirm Password" id="confirm_password"/>
+            <div class="imgBox d-flex justify-content-center align-items-center">
+            <img src="../img/lock.svg" alt="">
+            </div>
+        </div>
+        <div class="rememberForgot m-32 d-flex justify-content-evenly align-items-center">
+            <div class="d-flex">
+            <img class="curserPointer" src="../img/Check button.svg" alt="">
+            <span>I accept the <b class="blueFont">Privacy Policy</b></span>
+            </div>
+        </div>
+        <button class="btnDark m-32">Sign up</button>
+    </form>
     `;
 }
 
+
 function forgotPasswordTemplate() {
-    document.getElementById("loginBox").innerHTML = `
-    <h1>I forgot my password</h1>
+    document.getElementById("signUp").classList.add("d-none");
+    document.getElementById("loginBox").innerHTML = /*html*/ `
+    <div class="backImg">
+        <img  onclick="backToLogin()" src="../img/Vector.svg" alt="">
+        <h1>I forgot my password</h1>
+    </div> 
+    
     <div class="separator m-32"></div>
-    <p class="m-32">Don't worry! We will send you an email with the insturctions to reset your password.</p>
+    <span class="m-32">Don't worry! We will send you an email with the insturctions to reset your password.</span>
     <div class="inputFields m-32 d-flex justify-content-between align-items-center">
         <input required type="email" placeholder="Email"/>
         <div class="imgBox d-flex justify-content-center align-items-center">
@@ -56,11 +75,12 @@ function forgotPasswordTemplate() {
     `;
 }
 
+
 function resetPasswordTemplate() {
-    document.getElementById("loginBox").innerHTML = `
+    document.getElementById("loginBox").innerHTML = /*html*/ `
     <h1>Reset your password</h1>
     <div class="separator m-32"></div>
-    <p class="m-32">Change your account password</p>
+    <span class="m-32">Change your account password</span>
     <div class="inputFields m-32 d-flex justify-content-between align-items-center">
         <input required type="password" placeholder="New Password"/>
         <div class="imgBox d-flex justify-content-center align-items-center">
@@ -74,4 +94,15 @@ function resetPasswordTemplate() {
         </div>
     </div>
     `;
+}
+
+let password = document.getElementById("password");
+let confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
 }
