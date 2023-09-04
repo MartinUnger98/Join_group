b084d91358100253e0070114bf09ec4b69db35fd
Martin Unger <martin-unger2@live.at>
2023-09-04 11:26:42 +0200

    validate Password and set User Infos

--- js/login.js
+++ js/login.js
@@ -1,4 +1,3 @@
-
 function loggedIn() {
     window.location.href = "summary.html";
 }
@@ -10,126 +9,95 @@ function backToLogin() {
 }
 
 
-function signInTemplate() {
-    document.getElementById("signUp").classList.add("d-none");
-    document.getElementById("loginBox").innerHTML = /*html*/ `
-    <div class="backImg">
-        <img  onclick="backToLogin()" src="../img/Vector.svg" alt="">
-    </div> 
-    <form onsubmit="validatePassword(event)" class="widthLoginbox d-flex justify-content-center flex-column align-items-center">
-        <h1 class="h1WithBack">Sign Up</h1>
-        <div class="separator m-32"></div>
-        <div class="inputFields m-32 d-flex justify-content-between align-items-center">
-            <input required type="text" placeholder="Name"/>
-            <div class="imgBox d-flex justify-content-center align-items-center">
-            <img src="../img/person.svg" alt="">
-            </div>          
-        </div>
-        <div class="inputFields m-32 d-flex justify-content-between align-items-center">
-            <input required type="email" placeholder="Email"/>
-            <div class="imgBox d-flex justify-content-center align-items-center">
-            <img src="../img/mail.svg" alt="">
-            </div>          
-        </div>
-        <div class="inputFields m-32 d-flex justify-content-between align-items-center">
-            <input required type="password" placeholder="Password" id="password" pattern=".{8,}" title="Mindestens 8 Zeichen erforderlich"/>
-            <div class="imgBox d-flex justify-content-center align-items-center">
-            <img src="../img/lock.svg" alt="">
-            </div>
-        </div>
-        <div class="inputFields m-32 d-flex justify-content-between align-items-center" >
-            <input required type="password" placeholder="Confirm Password" id="confirm_password" pattern=".{8,}" title="Mindestens 8 Zeichen erforderlich"/>
-            <div class="imgBox d-flex justify-content-center align-items-center">
-            <img src="../img/lock.svg" alt="">
-            </div>
-        </div>
-        <div class="rememberForgot m-32 d-flex justify-content-evenly align-items-center">
-            <div class="d-flex gap-3" >
-                <div class="imgBox">
-                    <img id="checkSignIn" onclick="changeCheckBox('checkSignIn')" class="curserPointer" src="../img/Check button.svg" alt="">
-                </div>            
-                <span>I accept the <b class="blueFont">Privacy Policy</b></span>
-            </div>
-        </div>
-        <button class="btnDark m-32">Sign up</button>
-    </form>
-    `;
-}
-
-
-function forgotPasswordTemplate() {
-    let loginBox = document.getElementById("loginBox");
-    document.getElementById("signUp").classList.add("d-none");
-    loginBox.classList.add("loginBoxWide");
-    loginBox.classList.remove("loginBox");
-    loginBox.innerHTML = /*html*/ `
-    <form onsubmit="resetPasswordTemplate()" class="d-flex justify-content-center align-items-center flex-column m-32">
-        <div class="backImg">
-            <img  onclick="backToLogin()" src="../img/Vector.svg" alt="">        
-        </div> 
-        <h1 class="h1WithBack">I forgot my password</h1>    
-        <div class="separator m-32"></div>
-        <span class="m-32">Don't worry! We will send you an email with the insturctions to reset your password.</span>
-        <div class="inputFields m-32 d-flex justify-content-between align-items-center">
-            <input required type="email" placeholder="Email"/>
-            <div class="imgBox d-flex justify-content-center align-items-center">
-            <img src="../img/mail.svg" alt="">
-            </div>          
-        </div>
-        <button class="btnDark m-32">Send me the email</button>
-    </form>
-    `;
-}
-
-
-function resetPasswordTemplate() {
-    document.getElementById("loginBox").innerHTML = /*html*/ `
-    <form onsubmit="backToLogin()" class="d-flex justify-content-center align-items-center flex-column m-32">
-        <div class="backImg">
-            <img  onclick="backToLogin()" src="../img/Vector.svg" alt="">        
-        </div> 
-        <h1 class="h1WithBack">Reset your password</h1>
-        <div class="separator m-32"></div>
-        <span class="m-32">Change your account password</span>
-        <div class="inputFields m-32 d-flex justify-content-between align-items-center">
-            <input required type="password" placeholder="New Password" pattern=".{8,}" title="Mindestens 8 Zeichen erforderlich"/>
-            <div class="imgBox d-flex justify-content-center align-items-center">
-            <img src="../img/lock.svg" alt="">
-            </div>
-        </div>
-        <div class="inputFields m-32 d-flex justify-content-between align-items-center">
-            <input required type="password" placeholder="Confirm Password" pattern=".{8,}" title="Mindestens 8 Zeichen erforderlich"/>
-            <div class="imgBox d-flex justify-content-center align-items-center">
-            <img src="../img/lock.svg" alt="">
-            </div>
-        </div>
-        <button class="btnDark m-32">Continue</button>
-    </form>
-    `;
-}
-
 function changeCheckBox(id) {
     let box = document.getElementById(id);
     if (box.getAttribute('src') === "../img/Check button.svg") {
         box.src = "../img/checkButtenChecked.svg";
+        document.getElementById("signUpBtn").disabled = false;
     }
     else{
         box.src = "../img/Check button.svg";
+        document.getElementById("signUpBtn").disabled = true;
     }
 }
 
+
+function changeInputPasswordToTxt(idImg, idInput) {
+    let imgBox = document.getElementById(idImg);
+    let inputBox = document.getElementById(idInput);
+    switch(imgBox.getAttribute('src')) {
+        case "../img/lock.svg":
+            imgBox.src = "../img/visibility_off.svg";
+            break;
+        case "../img/visibility.svg":
+            imgBox.src ="../img/visibility_off.svg";
+            inputBox.type = "password";
+            break;
+        case "../img/visibility_off.svg":
+            imgBox.src ="../img/visibility.svg";
+            inputBox.type = "text";
+            break;
+    }    
+}
+
+
+function signUp() {
+    document.getElementById("signUp").classList.add("d-none");
+    document.getElementById("loginBox").innerHTML = signUpTemplate();
+}
+
+
+function forgotPasswordView() {
+    let loginBox = document.getElementById("loginBox");
+    document.getElementById("signUp").classList.add("d-none");
+    loginBox.classList.add("loginBoxWide");
+    loginBox.classList.remove("loginBox");
+    loginBox.innerHTML = forgotPasswordTemplate();
+}
+
+
+function resetPasswordView() {
+    document.getElementById("loginBox").innerHTML = resetPasswordTemplate();
+}
+
+
 function validatePassword(event) {
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirm_password").value;
-    const confirmPasswordInput = document.getElementById("confirm_password");
+    event.preventDefault(); // Verhindert das Standardverhalten des Formulars (Seitenneuladen)
+
+    const password = document.getElementById("signUpPassword").value;
+    const confirmPassword = document.getElementById("signUpConfirmPassword").value;
+    const confirmPasswordInput = document.getElementById("signUpConfirmPassword");
 
     if (password !== confirmPassword) {
         confirmPasswordInput.setCustomValidity("Passwort stimmt nicht überein");
     } else {
         confirmPasswordInput.setCustomValidity("");
+        setUserInfo();
     }
 
     confirmPasswordInput.addEventListener("input", function () {
         confirmPasswordInput.setCustomValidity(""); // Zurücksetzen der benutzerdefinierten Validierungsnachricht bei Änderung
     });
+}
+
+
+function setUserInfo() {
+    let key = 'userInfo' + usermail;
+    let username = document.getElementById("signUpName").value;
+    let usermail = document.getElementById("signUpEmail").value;
+    let userpassword = document.getElementById("signUpPassword").value;
+    let userInfo = {
+        name: username,
+        email: usermail,
+        password: userpassword
+    };
+    setItem(key, JSON.stringify(userInfo));
+}
+
+
+async function checkUserExist() {
+    let usermail = document.getElementById("signUpEmail").value;
+    let userInfo = await getItem('userInfo' + usermail);
+    let userInfoMail = JSON.parse(userInfo.data.value).email;
+
 }
