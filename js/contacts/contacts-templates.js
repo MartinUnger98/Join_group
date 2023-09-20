7497915e7a11bf02e6913f7370a0f34367794f4e
Raphael Janka <135700986+RaphaelJanka@users.noreply.github.com>
2023-09-20 11:12:56 +0200

    add Contact responsive complete

--- js/contacts/contacts-templates.js
+++ js/contacts/contacts-templates.js
@@ -23,7 +23,9 @@ function showNewContactEditor() {
     </div>
     <div class="ctcEditorRightSecond">
         <div class="closeBtn" onclick="closeEditorCtc()">
-            <img src="../img/closeEditor.png">
+            <svg width="13" height="14" viewBox="0 0 13 14"  xmlns="http://www.w3.org/2000/svg">
+                <path d="M6.24959 6.99984L11.4926 12.2428M1.00659 12.2428L6.24959 6.99984L1.00659 12.2428ZM11.4926 1.75684L6.24859 6.99984L11.4926 1.75684ZM6.24859 6.99984L1.00659 1.75684L6.24859 6.99984Z" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
         </div>
         <form onsubmit="addContact(); return false">
             <div class="inputCtcContainer"><input class="inputCtc" required id="name" type="text"
