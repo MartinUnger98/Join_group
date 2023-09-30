e101fa550bc3d4fb011be0d2749a0dbc07cc8baf
Raphael Janka <135700986+RaphaelJanka@users.noreply.github.com>
2023-09-30 13:57:55 +0200

    feedback3 fixed

--- js/board/board-editor.js
+++ js/board/board-editor.js
@@ -14,7 +14,7 @@ function openDetailedCardEditor(title, description, date, id, i) {
     editor.innerHTML = '';
     editor.innerHTML += showDetailedCardEditor(title, description, formattedDate, id, i);
     renderSubtasksInEditor(id, i);
-    renderUserInEditor(id, i);
+    renderUserInEditor(id);
     updateEditorWithSelectedPriorityBox(id, i);
     updateEditorWithMatchingContacts(id, i,);    
     setIDForEditAndindex(id, i);
@@ -218,7 +218,7 @@ function savedEditedTask(id, i) {
     let priority = getPriorityFromEditor(id);
     let contacts = getContactsFromEditor();
     let subtasks = getSubtasksFromEditor();
-    let bgContacts = getBgofContactFromEditor(id);
+    let bgContacts = getBgofContactFromEditor();
     pushEditedTask(newTask, input, textarea, date, priorityImg, priority, contacts, subtasks, bgContacts);
     hideDetailedTask();
     allMightyRender();
@@ -312,16 +312,22 @@ function getContactsFromEditor() {
  * @param {number} id - id of editor
  * @returns array with background-colors
  */
-function getBgofContactFromEditor(id) {
-    let bgColorsOfContactsFromEditor = [];
-    let checkedContact = document.querySelectorAll('.contact-selection.checked-editor');
-    checkedContact.forEach(contact => {
-        let contactId = contact.id; 
-        let i = parseInt(contactId.slice(-1));
-        let contactBgColor = window.getComputedStyle(document.getElementById(`contact-${id}-${i}`)).backgroundColor;
-        bgColorsOfContactsFromEditor.push(contactBgColor);
-    });
-    return bgColorsOfContactsFromEditor;
+function getBgofContactFromEditor() {
+    let selectedContactsFromEditor = getContactsFromEditor();
+    let bgContacts = [];
+
+    for (let i = 0; i < selectedContactsFromEditor.length; i++) {
+        let selectedContact = selectedContactsFromEditor[i];
+        for (let j = 0; j < allContacts.length; j++) {
+            let contact = allContacts[j];
+            if (contact.name === selectedContact) {
+                bgContacts.push(contact.bgColor);
+                break;
+            }
+        }
+    }
+
+    return bgContacts;
 }
 
 
@@ -338,204 +344,4 @@ function getSubtasksFromEditor() {
     return subtasks;
 }
 
-
-/**
- * This function renders all users within the contact dropdown in editor
- * @param {number} id - id of editor
- */
-function renderUserInEditor(id, i) {
-    let content = document.getElementById(`contact_content-editor${id}`);
-    content.innerHTML = '';
-    sortByFirstName();
-    for (let i = 0; i < allContacts.length; i++) {
-        const user = allContacts[i];
-        let username = user.name;
-        const initials = getInitials(username);
-        const bgUser = user.bgColor;
-        content.innerHTML += showAllContactsinEditor(id, i, initials, bgUser, username);
-    };
-}
-
-
-/**
- * This function toggles the the selected contacts in editor
- * @param {number} id - id of editor
- * @param {number} i - index of tasks (used as id for contact selection)
- */
-function toggleCheckboxInEditor(id, i) {
-    let selection = document.getElementById(`user-selection-${id}-${i}`);
-    if (!selection.classList.contains('checked-editor')) {
-        selection.classList.add('checked-editor');
-        moveSelectedContactsInEditor(id)
-    } else {
-        selection.classList.remove('checked-editor');
-        moveSelectedContactsInEditor(id);
-    }
-}
-
-
-/**
- * This function moves the selected contacts into the div underneath
- * @param {number} id - id of editor
- */
-function moveSelectedContactsInEditor(id) {
-    let selectedContactsDiv = document.getElementById(`selected_contacts_editor-${id}`);
-    selectedContactsDiv.innerHTML = ''; 
-    for (let i = 0; i < allContacts.length; i++) {
-        if (isContactSelectedInEditor(id, i))
-        showSelectedContactsInEditor(selectedContactsDiv, id, i);
-    }
-}
-
-
-/**
- * This function generates the condition for the function moveSelectedContactsInEditor()
- * @param {number} id - id of detailed task editor
- * @param {number} i - index of tasks
- * @returns - condition
- */
-function isContactSelectedInEditor(id, i) {
-    let selection = document.getElementById(`user-selection-${id}-${i}`);
-    let checkbox = document.getElementById(`user-editor${id}-${i}`);  
-    return selection.classList.contains('checked-editor') && checkbox.checked;
-}
-
-
-/**
- * This function shows the selected contacts inside of the div 
- * @param {string} selectedContactsDiv - div of added contacts
- * @param {number} id - id of editor
- * @param {number} i - index of allContacts
- */
-function showSelectedContactsInEditor(selectedContactsDiv, id, i) {
-    const user = allContacts[i];
-    let username = user.name;
-    const initials = getInitials(username);
-    const bgUser = user.bgColor;
-    selectedContactsDiv.innerHTML += /*html*/ `
-        <div class="initials-selected" id="selected_contact-${id}-${i}" style="background-color: ${bgUser}">${initials}</div>
-    `;
-}
-
-
-/**
- * This function switches the border-color back and closes the dropdown, if contacts were added
- * @param {number} id - id of editor
- */
-function switchBorderandDropdownOfContacts(id) {
-    let border = document.getElementById(`contact_dropdown-${id}`);
-    let dropDown = document.getElementById(`drop_1-${id}`);
-    border.classList.remove('border-color');
-    dropDown.classList.remove('switch');
-}
-
-
-/**
- * This function determines the priorities and priority-imgages of in editor
- * @param {number} id - id of editor
- * @param {number} i - index of tasks
- */
-function updateEditorWithSelectedPriorityBox(id, i) {
-    const task = tasks[i]
-    let priority = task.priority;
-    let urgent = document.getElementById(`urgent-${id}`);
-    let medium = document.getElementById(`medium-${id}`);
-    let low = document.getElementById(`low-${id}`);
-    let urgentImg = document.getElementById(`urgent-img-${id}`);
-    let mediumImg = document.getElementById(`medium-img-${id}`);
-    let lowImg = document.getElementById(`low-img-${id}`);
-    highlightPriorityBoxes(priority, urgent, urgentImg, medium, mediumImg, low, lowImg);
-}
-
-
-/**
- * This function highlights the priority-box in editor, which has the same priority as in the array tasks 
- * @param {string} priority - priority in task
- * @param {string} urgent - id of urgent-box in editor
- * @param {string} urgentImg - id of urgent-img in editor
- * @param {string} medium - id of medium in editor
- * @param {string} mediumImg - id of medium-img in editor
- * @param {string} low - id of low-box in editor
- * @param {string} lowImg -id of low-img in editor
- */
-function highlightPriorityBoxes(priority, urgent, urgentImg, medium, mediumImg, low, lowImg) {
-    if (priority) {
-        if (priority === 'Urgent') {
-            switchUrgent(urgent, urgentImg);
-        } else if (priority === 'Medium') {
-            switchMedium(medium, mediumImg);
-        } else if (priority === 'Low') {
-            switchLow(low, lowImg);
-        }
-    }
-}
-
-
-/**
- * This function compares the added contacts of each task with the selection of the contacts dropdown in editor
- * @param {number} id - id of editor
- * @param {number} i - index of task
- */
-function updateEditorWithMatchingContacts(id, i) {
-    const taskContacts = tasks[i].contacts; // Kontakte aus der Aufgabe
-    if (!taskContacts) {
-        return; // Keine Aufgabenkontakte vorhanden, nichts zu überprüfen
-    }
-    for (let j = 0; j < allContacts.length; j++) {
-        const user = allContacts[j];
-        const username = user.name;
-        if (taskContacts.includes(username)) {
-            handleMatchingContact(id, j, username, user);
-        }
-    }
-}
-
-
-/**
- * This function updates the added contacts of each task with the selection of the contacts dropdown in editor
- * @param {number} id - id of editor
- * @param {number} j - index of allContacts
- * @param {string} username - name of contacts in 
- * @param {string} user - array allContacts
- */
-function handleMatchingContact(id, j, username, user) {
-    const initials = getInitials(username);
-    const bgUser = user.bgColor;
-    const selection = document.getElementById(`user-selection-${id}-${j}`);
-    const checkbox = document.getElementById(`user-editor${id}-${j}`);
-    if (!selection.classList.contains('checked-editor')) {
-        selection.classList.add('checked-editor');
-    }
-    checkbox.checked = true;
-    showExistingContactsInEditor(id, j, initials, bgUser);
-}
-
-
-/**
- * This function renders the existing contacts within the selected-contacts-div
- * @param {number} id - id of editor
- * @param {number} i - index of allContacts
- * @param {string} initials - initials of contact
- * @param {string} bgUser - background-color of contact
- */
-function showExistingContactsInEditor(id, i, initials, bgUser) {
-    let selectedContactsDiv = document.getElementById(`selected_contacts_editor-${id}`);
-    let selection = document.getElementById(`user-selection-${id}-${i}`);
-    let checkbox = document.getElementById(`user-editor${id}-${i}`);  
-    if (selection.classList.contains('checked-editor') && checkbox.checked) {
-        selectedContactsDiv.innerHTML += /*html*/ `
-            <div class="initials-selected" id="selected_contact-${id}-${i}" style="background-color: ${bgUser}">${initials}</div>
-        `;
-    }
-}
-
-
-/**
- * This function opens the add new contact card (-->addTask.js) and toggles the editor dropdown
- * @param {number} id - id of detailed task
- * @param {number} i - index of tasks
- */
-function addNewContactsInEditor() {
-    showContactEditor();
-}
 
